c8e451caf28ba86a1284a1463faf84da99227c08
Kaan Kilic <kilickaan1998@gmail.com>
2018-11-12 13:02:28 +0100

    :sparkles: Updates isp template to match new tutorial

--- alexa/isp/app/app.js
+++ alexa/isp/app/app.js
@@ -19,14 +19,37 @@ const app = new App(config);
 
 app.setHandler({
     'LAUNCH': function() {
-        this.ask('Which Skill Item do you want to buy?');
+        this.toIntent('UpsellIntent');
     },
-
+    'UpsellIntent': function() {
+        let productReferenceName = 'frozen_sword';
+        this.alexaSkill()
+        .inSkillPurchase()
+        .getProductByReferenceName(productReferenceName, (error, product) => {
+            if (error) {
+                console.log(error);
+            }
+            if (product.entitled === 'ENTITLED') {
+                this.tell('You have already bought this item.');
+                return;
+            } else {
+                let prompt = 'The frozen sword will help you on your journey. Are you interested?';
+                let token = 'testToken';
+                this.alexaSkill().inSkillPurchase().upsell(product.productId, prompt, token);
+            }
+        });
+    },
     'BuySkillItemIntent': function(productName) {
+        if (!productName) {
+            this.ask('You can choose either the premium pass. or frozen sword. Which are you interested in?');
+        }
         let productReferenceName = productName.id;
         this.alexaSkill()
             .inSkillPurchase()
             .getProductByReferenceName(productReferenceName, (error, product) => {
+                if (error) {
+                    console.log(error);
+                }
                 if (product.entitled === 'ENTITLED') {
                     this.tell('You have already bought this item.');
                     return;
@@ -34,29 +57,33 @@ app.setHandler({
                 this.alexaSkill().inSkillPurchase().buy(product.productId);
             });
     },
-
     'RefundSkillItemIntent': function(productName) {
         let productReferenceName = productName.id;
         this.alexaSkill()
             .inSkillPurchase()
             .getProductByReferenceName(productReferenceName, (error, product) => {
+                if (error) {
+                    console.log(error);
+                    // Continue, where you left off
+                }
                 if (product.entitled !== 'ENTITLED') {
                     this.tell('You have not bought this item yet.');
-                } else {
-                    this.alexaSkill().inSkillPurchase().cancel(product.productId);
                 }
+                this.alexaSkill().inSkillPurchase().cancel(product.productId);
             });
     },
-
     'ON_PURCHASE': function() {
-        console.log('Payload:', this.alexaSkill().inSkillPurchase().getPayload());
-        this.tell(this.alexaSkill().inSkillPurchase().getPurchaseResult());
+        const name = this.request().name;
+        const productId = this.alexaSkill().inSkillPurchase().getProductId();
+        const purchaseResult = this.alexaSkill().inSkillPurchase().getPurchaseResult();
+        const token = this.request().token;
+    
+        if (purchaseResult === 'ACCEPTED') {
+            this.tell('Great! Let\'s use your new item');
+        } else {
+            this.tell('Okay. Let\'s continue where you left off.');
+        }
     },
-
-    'Unhandled': function() {
-        this.toIntent('LAUNCH');
-    },
-
 });
 
 module.exports.app = app;
