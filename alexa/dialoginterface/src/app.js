7563aac27eb20ffe387104b1d3dfbcbb3170665b
Kaan Kilic <kilickaan1998@gmail.com>
2018-12-10 13:29:02 +0100

    :construction: v2

--- alexa/dialoginterface/src/app.js
+++ alexa/dialoginterface/src/app.js
@@ -19,15 +19,14 @@ app.setHandler({
         this.ask('Where are you flying to?', 'Where are you flying to?');
     },
     SearchFlightIntent() {
-        console.log(this.$inputs);
         if (!this.$alexaSkill.$dialog.isCompleted()) {
             this.$alexaSkill.$dialog.delegate();
         } else if (!this.$inputs.ticketCount.value) {
-            this.$alexaSkill.$dialog.elicitSlot('ticketCount', 'How many tickets do you need?');
+            this.$alexaSkill.$dialog.elicitSlot('ticketCount', 'How many tickets do you need?', 'How many tickets do you need?');
         } else if (this.$alexaSkill.$dialog.getIntentConfirmationStatus() !== 'CONFIRMED') {
-            let speech = `So you are flying from ${this.$inputs.fromCity.value} to ${this.$inputs.toCity.value}
-                        on ${this.$inputs.date.value} and you need ${this.$inputs.ticketCount.value} tickets, right?`;
-            this.$alexaSkill.$dialog.confirmIntent(speech, speech);
+            let speech = `So you are flying from ${this.$inputs.fromCity.value} to ${this.$inputs.toCity.value} on ${this.$inputs.date.value} and you need ${this.$inputs.ticketCount.value} tickets, right?`;
+            let reprompt = speech;
+            this.$alexaSkill.$dialog.confirmIntent(speech, reprompt);
         } else if (this.$alexaSkill.$dialog.getIntentConfirmationStatus() === 'CONFIRMED') {
             let flightData = {
                 fromCity: this.$inputs.fromCity.value,
@@ -39,6 +38,7 @@ app.setHandler({
         }
     },
     FlightApiIntent(flightData) {
+        console.log('flightData: ');
         console.log(flightData);
         this.tell('Your flight is booked, thanks!');
     },
