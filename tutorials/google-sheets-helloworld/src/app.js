c2a98882ebbd6c22813d167d0b09a28d6d12dda3
Jan König <jankoenig120190@googlemail.com>
2018-12-19 10:53:42 +0100

    ✨ Add Spreadsheet Example Code

---
+++ tutorials/google-sheets-helloworld/src/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const {App} = require('jovo-framework');
+const {Alexa} = require('jovo-platform-alexa');
+const {GoogleAssistant} = require('jovo-platform-googleassistant');
+const {JovoDebugger} = require('jovo-plugin-debugger');
+const {FileDb} = require('jovo-db-filedb');
+const { GoogleSheetsCMS } = require('jovo-cms-googlesheets');
+
+
+const app = new App();
+
+app.use(
+    new Alexa(),
+    new GoogleAssistant(),
+    new JovoDebugger(),
+    new FileDb(),
+    new GoogleSheetsCMS()
+);
+
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+    LAUNCH() {
+        this.toIntent('HelloWorldIntent');
+    },
+
+    HelloWorldIntent() {
+        this.ask(this.t('welcome.speech'), this.t('welcome.reprompt'));
+    },
+
+    MyNameIsIntent() {
+        this.tell(this.t('greeting.speech', { name: this.$inputs.name.value }));
+    },
+});
+
+module.exports.app = app;
