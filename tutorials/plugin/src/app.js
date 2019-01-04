fc1e4c723329334a525295b239470dbbf455431b
Kaan Kilic <kilickaan1998@gmail.com>
2019-01-04 14:10:30 +0100

    :sparkles: Updates custom logger to v2

--- tutorials/plugin/src/app.js
+++ tutorials/plugin/src/app.js
@@ -5,7 +5,6 @@
 // ------------------------------------------------------------------
 
 const {App} = require('jovo-framework');
-const {Plugin} = require('jovo-core');
 const {Alexa} = require('jovo-platform-alexa');
 const {GoogleAssistant} = require('jovo-platform-googleassistant');
 const {JovoDebugger} = require('jovo-plugin-debugger');
@@ -19,29 +18,22 @@ class CustomLogging {
         this.output = '';
     }
     install(app) {
-        app.on('response', this.saveOutput.bind(this));
-        app.on('nlu', this.log.bind(this));
+        app.middleware('platform.output').use(this.saveOutput.bind(this));
+        app.middleware('platform.nlu').use(this.log.bind(this));
     }
     saveOutput(handleRequest) {
-        if (!handleRequest.jovo) {
-            return;
-        }
-
         if (handleRequest.jovo.constructor.name === 'GoogleAction') {
-            let speech = handleRequest.jovo.$response.getSpeech().replace(/<[^>]*>/g, '');
+            let speech = handleRequest.jovo.$response.getSpeech().replace(/<\/?speak\/?>/g, '');
             let reprompt;
             if (handleRequest.jovo.$response.getReprompt()) {
-                reprompt = handleRequest.jovo.$response.getReprompt().replace(/<[^>]*>/g, '');
+                reprompt = handleRequest.jovo.$response.getReprompt().replace(/<\/?speak\/?>/g, '');
             }
+            let state = handleRequest.jovo.getState() || '-';
             this.output = `\nspeech: ${speech} | reprompt: ${reprompt}\n`
-            this.output += `\nState: ${handleRequest.jovo.getState() || '-'} | `;
+            this.output += `\nState: ${state} | `;
         }
     }
     log(handleRequest) {
-        if (!handleRequest.jovo) {
-            return;
-        }
-
         if (handleRequest.jovo.constructor.name === 'GoogleAction') {
             let intentName = handleRequest.jovo.$request.getIntentName();
             if (intentName === 'Default Fallback Intent') {
