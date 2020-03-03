f8e94ffe99fe7e7c506eb5beb36a5e084120a619
rubenaeg <email@aegerter.tech>
2020-03-03 16:07:36 +0100

    :sparkles: Add Bixby AudioPlayer Template

--- bixby/02_audioplayer/javascript/src/app.js
+++ bixby/02_audioplayer/javascript/src/app.js
@@ -7,35 +7,51 @@
 const { App } = require('jovo-framework');
 const { Alexa } = require('jovo-platform-alexa');
 const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { Bixby } = require('jovo-platform-bixby');
 const { JovoDebugger } = require('jovo-plugin-debugger');
 const { FileDb } = require('jovo-db-filedb');
 
 const app = new App();
 
 app.use(
-    new Alexa(),
-    new GoogleAssistant(),
-    new JovoDebugger(),
-    new FileDb()
+	new Alexa(),
+	new GoogleAssistant(),
+	new Bixby(),
+	new JovoDebugger(),
+	new FileDb()
 );
-
 
 // ------------------------------------------------------------------
 // APP LOGIC
 // ------------------------------------------------------------------
 
 app.setHandler({
-    LAUNCH() {
-        return this.toIntent('HelloWorldIntent');
-    },
+	LAUNCH() {
+		return this.toIntent('HelloWorldIntent');
+	},
 
-    HelloWorldIntent() {
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-    },
+	HelloWorldIntent() {
+		this.ask("Hello World! What's your name?", 'Please tell me your name.');
+	},
 
-    MyNameIsIntent() {
-        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
-    },
+	MyNameIsIntent() {
+		this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+	},
+
+	PlayAudioIntent() {
+		this.$bixbyCapsule.$audioPlayer.play({
+			title: 'Example Audio',
+			stream: { url: 'https://s3.amazonaws.com/jovo-songs/song1.mp3' }
+		});
+	},
+
+	AUDIOPLAYER: {
+		'BixbyCapsule.AudioPlaying'() {
+			console.log('BixbyCapsule.AudioPlaying');
+
+			this.tell('Playing audio.');
+		}
+	}
 });
 
-module.exports.app = app;
+module.exports = { app };
