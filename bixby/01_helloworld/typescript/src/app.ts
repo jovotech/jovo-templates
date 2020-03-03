37a075e36434a44582e220332afe80118c8ca064
rubenaeg <email@aegerter.tech>
2020-03-03 16:00:56 +0100

    :sparkles: Add Bixby TypeScript HelloWorld Template

--- bixby/01_helloworld/typescript/src/app.ts
+++ bixby/01_helloworld/typescript/src/app.ts
@@ -1,8 +1,9 @@
-import {App} from 'jovo-framework';
-import {Alexa} from 'jovo-platform-alexa';
-import {JovoDebugger} from 'jovo-plugin-debugger';
-import {FileDb} from 'jovo-db-filedb';
-import {GoogleAssistant} from 'jovo-platform-googleassistant';
+import { App } from 'jovo-framework';
+import { Alexa } from 'jovo-platform-alexa';
+import { GoogleAssistant } from 'jovo-platform-googleassistant';
+import { Bixby } from 'jovo-platform-bixby';
+import { JovoDebugger } from 'jovo-plugin-debugger';
+import { FileDb } from 'jovo-db-filedb';
 
 // ------------------------------------------------------------------
 // APP INITIALIZATION
@@ -11,29 +12,29 @@ import {GoogleAssistant} from 'jovo-platform-googleassistant';
 const app = new App();
 
 app.use(
-    new Alexa(),
-    new GoogleAssistant(),
-    new JovoDebugger(),
-    new FileDb(),
+	new Alexa(),
+	new GoogleAssistant(),
+	new Bixby(),
+	new JovoDebugger(),
+	new FileDb()
 );
-
 
 // ------------------------------------------------------------------
 // APP LOGIC
 // ------------------------------------------------------------------
 
 app.setHandler({
-    LAUNCH() {
-        return this.toIntent('HelloWorldIntent');
-    },
+	LAUNCH() {
+		return this.toIntent('HelloWorldIntent');
+	},
 
-    HelloWorldIntent() {
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-    },
+	HelloWorldIntent() {
+		this.ask("Hello World! What's your name?", 'Please tell me your name.');
+	},
 
-    MyNameIsIntent() {
-        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
-    },
+	MyNameIsIntent() {
+		this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+	}
 });
 
-export {app};
+export { app };
