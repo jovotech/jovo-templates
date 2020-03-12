f60af673bfe720cc0acd33b862abef7f3340984b
Kaan Kilic <kilickaan1998@gmail.com>
2020-03-12 10:50:12 +0100

    :sparkles: Autopilot handoff template

---
+++ autopilot/handoff/javascript/README.md
@@ -0,0 +1,18 @@
+[![Jovo Framework](https://www.jovo.tech/img/github-logo.png)](https://www.jovo.tech)
+
+<p align="center">Templates for the <a href="https://github.com/jovotech/jovo-framework-nodejs">Jovo Framework</a> ⭐️</p>
+
+<p align="center">
+<a href="https://www.jovo.tech/framework/docs/"><strong>Documentation</strong></a> -
+<a href="https://github.com/jovotech/jovo-cli"><strong>CLI </strong></a> - <a href="https://github.com/jovotech/jovo-framework-nodejs/blob/master/CONTRIBUTING.md"><strong>Contributing</strong></a> - <a href="https://twitter.com/jovotech"><strong>Twitter</strong></a></p>
+<br/>
+
+# Template: Handoff on Twilio Autopilot
+
+Jovo Sample Autopilot Bot that can handoff the session to a human agent using Twilio Flex.
+
+```sh
+$ jovo new <directory> --template autopilot/handoff
+```
+
+After that you have add your Workflow's ID to the marked section in the handler.
---
+++ autopilot/handoff/javascript/src/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const { App } = require('jovo-framework');
+const { Autopilot } = require('jovo-platform-twilioautopilot');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+const app = new App();
+
+app.use(
+    new Autopilot(),
+    new JovoDebugger(),
+    new FileDb(),
+);
+
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+    LAUNCH() {
+        return this.ask('Do you want me to hand over the conversation to a human?');
+    },
+
+    YesIntent() {
+        const request = this.$request.toJSON();
+
+        if (request.Channel !== 'voice') {
+            return this.tell('I am sorry, the handoff feature is only supported for voice channels.')
+        } else {
+            return this.$autopilotBot.setActions([
+                {
+                    handoff: {
+                        channel: "voice",
+                        uri: "taskrouter://<your-workflow-id>" // add your Workflow ID here
+                    },
+                }
+            ])
+        }
+    },
+
+    NoIntent() {
+        return this.tell('Ok, maybe next time');
+    },
+});
+
+module.exports.app = app;
