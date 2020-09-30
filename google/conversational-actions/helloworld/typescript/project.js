2d0de55ede4cfe75a2cd925b2f32bfc36bdf9cfc
aswetlow <a.swetlow@gmail.com>
2020-09-30 14:32:10 +0200

    :sparkles: First Conversational Actions template

---
+++ google/conversational-actions/helloworld/typescript/project.js
@@ -0,0 +1,10 @@
+// ------------------------------------------------------------------
+// JOVO PROJECT CONFIGURATION
+// ------------------------------------------------------------------
+
+module.exports = {
+  googleAction: {
+    nlu: 'dialogflow',
+  },
+  endpoint: '${JOVO_WEBHOOK_URL}',
+};
